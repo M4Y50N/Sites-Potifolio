77342ffc977d4617d28143de6ce874cb39da5fc9
M4Y50N <pethersonreis@gmail.com>
2021-02-11 18:35:33 -0300

    Projeto da urna
    Toda  a parte do layout da urna foi feita

---
+++ assets/sites-html/site-01/assets/js/etapas.js
@@ -0,0 +1,25 @@
+const etapas = [
+	{
+		titulo: "VEREADOR",
+		numeros: 5,
+		candidatos: [
+			{
+				numero: 38111,
+				nome: "Fulano de Tal",
+				partido: "ABC",
+				fotos: [{ url: "38111.png", legenda: "Vereador", small: false }],
+			},
+			{
+				numero: 77222,
+				nome: "Beltrano da Silva",
+				partido: "DEFG",
+				fotos: [{ url: "77222.png", legenda: Vereador, small: false }],
+			},
+		],
+	},
+	{
+		titulo: "PREFEITO",
+		numeros: 2,
+		candidatos: [{}],
+	},
+];
