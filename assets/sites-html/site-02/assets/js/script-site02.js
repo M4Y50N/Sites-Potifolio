27e1152cc4e7a01d6afdb0687ff4ce0b81c1eca6
M4Y50N <pethersonreis@gmail.com>
2021-03-06 12:06:43 -0300

    Section services
    Fiz uma mudança no layout da parte do carousel na seção, e adicionei alguns efeitos de mouse hover

--- assets/sites-html/site-02/assets/js/script-site02.js
+++ assets/sites-html/site-02/assets/js/script-site02.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll", function () {
 	let header = document.querySelector("header");
 	header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+document.querySelectorAll(".prev").forEach((x) => {
+	x.addEventListener("mouseover", () => {
+		x.firstElementChild.classList.toggle("show");
+		x.querySelector(".img").classList.toggle("img-hover");
+	});
+	x.addEventListener("mouseout", () => {
+		x.firstElementChild.classList.toggle("show");
+		x.querySelector(".img").classList.toggle("img-hover");
+	});
+});
