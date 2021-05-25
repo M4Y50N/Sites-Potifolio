ccfdf7806b37cf58757b2cdf5cf5ba0562271c87
M4Y50N <pethersonreis@gmail.com>
2021-05-25 18:22:51 -0200

    update

--- assets/sites-html/site-01/assets/js/gulpfile.js
+++ assets/sites-html/site-01/assets/js/gulpfile.js
@@ -1,1 +1,14 @@
-const gulp;
+const gulp = require("gulp"),
+	uglify = require("gulp-uglify"),
+	rename = require("gulp-rename"),
+	sourcemaps = require("gulp-sourcemaps"),
+	runSequence = require("run-sequence"),
+    plumber = require("gulp-plumber")
+
+var onError = function(err){
+     console.log(err)
+}
+
+gulp.task("compress", () => {
+	return gulp.src("./script.js").pipe(uglify()).pipe(gulp.dest("dist"));
+});
