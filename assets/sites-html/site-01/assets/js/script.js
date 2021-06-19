2134aedb403c494bdb2bc9a2af9f66b40b3b7372
M4Y50N <pethersonreis@gmail.com>
2021-06-19 20:20:41 -0200

    Site 1

--- assets/sites-html/site-01/assets/js/script.js
+++ assets/sites-html/site-01/assets/js/script.js
@@ -1,2 +1,33 @@
-var x = 1,
-	y = "z";
+// app screenshots
+let featurelist = document.querySelectorAll(".feature-content ul li"),
+	featureimgs = document.querySelectorAll(".feature-img img"),
+	featureimgslist = [];
+
+featureimgs.forEach((x, index) => {
+	if (index > 0) {
+		featureimgslist.push(x);
+	}
+});
+
+featurelist.forEach((x, index) => {
+	let icon = x.querySelector("i");
+
+	x.addEventListener("mouseover", (event) => {
+		icon.style.color = "white";
+		icon.style.backgroundColor = "#65ccd1";
+
+		featureimgslist[index].classList.add("show");
+		featureimgslist.forEach((x) => {
+			if (x.classList == "show" && featureimgslist[index] != x) {
+				x.classList.remove("show");
+			}
+		});
+	});
+
+	x.addEventListener("mouseout", (event) => {
+		icon.style.color = "#65ccd1";
+		icon.style.backgroundColor = "white";
+	});
+});
+
+/////////////////////////////////////////////////////////
